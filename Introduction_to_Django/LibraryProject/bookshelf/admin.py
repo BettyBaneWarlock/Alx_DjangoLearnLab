a7e63f96522730adbf980a68dc4fd6b6282712f3
Betel Dessalegn <beteldessalegn0147@gmail.com>
2024-11-10 15:52:14 +0300

    task 2

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ('title', 'author', 'publication_year')
+    # Add filter options for publication year
+    list_filter = ('publication_year',)
+    # Enable search functionality on title and author fields
+    search_fields = ('title', 'author')
+
+# Register the Book model with the custom BookAdmin configuration
+admin.site.register(Book, BookAdmin)
