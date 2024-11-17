b31633f060b1de7d8115f603833da832b13c686a
Betel Dessalegn <beteldessalegn0147@gmail.com>
2024-11-17 17:22:10 +0300

    week 10 q1 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Library
 
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()  # Retrieve all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 # Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
