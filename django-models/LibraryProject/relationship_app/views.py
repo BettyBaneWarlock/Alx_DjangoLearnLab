bc64ff01b796e91371520d87355593df363a43bc
Betel Dessalegn <beteldessalegn0147@gmail.com>
2024-11-17 17:19:12 +0300

    week 10 q1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Library
 
-# Create your views here.
+# Class-based view for library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
