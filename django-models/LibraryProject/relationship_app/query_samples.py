5ac2b09677e8e86236c224b8f221a4e3ee63287c
Betel Dessalegn <beteldessalegn0147@gmail.com>
2024-11-17 17:12:47 +0300

    week 10 q0 3

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -12,5 +12,5 @@ def get_books_in_library(library_name):
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    library = Library.objects.get(name=library_name)  # Get the library object
+    return Librarian.objects.get(library=library)  # Retrieve the librarian
