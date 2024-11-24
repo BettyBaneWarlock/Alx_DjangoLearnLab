5b0d419d39fc68f4a3b03dc753cfde730ff7a0f6
Betel Dessalegn <beteldessalegn0147@gmail.com>
2024-11-24 16:53:47 +0300

    w11 q0

---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import list_books, LibraryDetailView, UserLoginView, UserLogoutView, register, add_book, edit_book, delete_book
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),  # URL for the function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for the class-based view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete/<int:book_id>/', delete_book, name='delete_book'),
+]
